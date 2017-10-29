d8e3837a3841a8fe46912facf98fd1a7f05f4873
jackykc <carrotnolie@gmail.com>
2017-10-29 00:23:25 -0600

    modified url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,7 +26,7 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^graphiql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    url(r'^gql', csrf_exempt(GraphQLView.as_view(batch=True))),
+    url(r'^gql', csrf_exempt(GraphQLView.as_view(graphiql=False))),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
